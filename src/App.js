b1ab953c2afcd7a5f66a836321441178d289cddf
ferrouklilia <125090363+liliaferrouk@users.noreply.github.com>
2023-04-23 20:44:11 +0100

    v1

---
+++ src/App.js
@@ -0,0 +1,128 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor"
+import { data } from "./data"
+import Split from "react-split"
+// we have to install ract-split: npm i react-split
+import { nanoid } from "nanoid"
+
+//what we're gonna do in this challenge:
+
+//1- sync notes with localStorage
+//localStorage.getItem("key") + localStorag.setItem("key",value) #value should be an array
+//Json.stringify(value) + Json.parse(stringifiesValue) if value is not an array
+
+//2- add note summary titles
+//using: string.split("\n")[0]
+
+//3- Move modified notes to the top of the list
+//using: newarray.unshift(modifiedElem)
+
+//4- delete notes
+//using filter(true): will return it, filter(false): will no return it (delete it)
+
+export default function App() {
+  //using lazy initialize so that it will render only the first time not everytime app get renderder
+  const [notes, setNotes] = React.useState( () => 
+      {return (
+           JSON.parse(localStorage.getItem("notes")) || []
+        )
+      })
+
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+      (notes[0] && notes[0].id) || ""
+  )
+
+  React.useEffect(()=> {
+       localStorage.setItem("notes",JSON.stringify(notes))
+  },[notes])
+
+  function createNewNote() {
+      const newNote = {
+          id: nanoid(),
+          body: "# Type your title here"
+      }
+      setNotes(prevNotes => [newNote, ...prevNotes])
+      setCurrentNoteId(newNote.id)
+  }
+
+  function updateNote(text) {
+    //rearrange the most recently-modified on the top:
+      setNotes(oldNotes => {
+        const newarray = []
+        for(let i=0; i<oldNotes.length; i++){
+            const oldNote = oldNotes[i]
+            if(oldNote.id === currentNoteId){
+                 newarray.unshift({...oldNote, body: text})
+            }
+            else{
+                newarray.push(oldNote)
+            }
+        }
+        return newarray
+      })
+      //this does not rearrange the most recently-modified on the top
+    //   setNotes(oldNotes => oldNotes.map(oldNote =>{
+    //     return oldNote.id === currentNoteId 
+    //           ? {...oldNote, body: text}
+    //           : oldNote
+    //   }))
+  }
+
+  function deleteNote(event, noteId){
+    event.stopPropagation()
+    setNotes(oldNotes => {
+        return(
+            //th filter will only return the notes that id is diffrent than the noteId wee want to delete
+            oldNotes.filter(note => note.id !== noteId )
+        )
+    })
+  }
+
+  function findCurrentNote() {
+      return notes.find(note => {
+          return note.id === currentNoteId
+      }) || notes[0]
+  }
+
+  return (
+      <main>
+          {
+              notes.length > 0
+                  ?
+                  <Split
+                      sizes={[30, 70]}
+                      direction="horizontal"
+                      className="split"
+                  >
+                      <Sidebar
+                          notes={notes}
+                          currentNote={findCurrentNote()}
+                          setCurrentNoteId={setCurrentNoteId}
+                          newNote={createNewNote}
+                          deleteNote={deleteNote}
+                      />
+                      {
+                          currentNoteId &&
+                          notes.length > 0 &&
+                          <Editor
+                              currentNote={findCurrentNote()}
+                              updateNote={updateNote}
+                          />
+                      }
+                  </Split>
+                  :
+                  <div className="no-notes">
+                      <h1>You have no notes</h1>
+                      <button
+                          className="first-note"
+                          onClick={createNewNote}
+                      >
+                          Create one now
+                      </button>
+                  </div>
+          }
+      </main>
+  )
+}
+
---
+++ src/components/Editor.js
@@ -0,0 +1,32 @@
+import React from "react"
+// we should install them: npm i react-mde --legacy-peer-deps
+import ReactMde from "react-mde"
+import Showdown from "showdown"
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+export default function Editor({ currentNote, updateNote }) {
+    const [selectedTab, setSelectedTab] = React.useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })  
+
+    return (
+        <section className="pane editor">
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </section>
+    )
+}
